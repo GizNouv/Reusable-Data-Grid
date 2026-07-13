2312ca60a9716654085376254ed0b625703c9910
GizNouv <giznouv@gmail.com>
2026-07-13 18:47:22 +0330

    upd: add suspense boundary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { DataGridDemo } from "@/components/demo/DataGridDemo";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-50 lg:p-8 p-4">
       <h1 className="text-2xl font-bold text-center mb-8">📊 Reusable Data Grid</h1>
-      <DataGridDemo />
+      <Suspense fallback={'Loading...'}>
+        <DataGridDemo />
+      </Suspense>
     </main>
   );
 }
